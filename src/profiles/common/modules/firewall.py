2728c0b64f5110f23e46cd69b66fb017afa54064
Krisztian Litkey <krisztian.litkey@intel.com>
2018-01-04 17:55:00 +0200

    WiP: working on simplistic firewall generation.

--- src/profiles/common/modules/firewall.py
+++ src/profiles/common/modules/firewall.py
@@ -1,12 +1,45 @@
 #!/usr/bin/env python3
 
+import ipaddress
 from genconfig.parser import *
 
 class Firewall(Node):
     def __init__(self, nodedef, root, parent, node_tkn):
         Node.__init__(self, nodedef, root, parent, node_tkn)
         self.node_tkn = node_tkn
-        self.isolations = []
+        self.protected = []
+        self.isolated = []
+        self.trusted_interfaces = []
+        self.trusted_networks = []
+        self.trusted_hosts = []
+        self.rules = []
+
+    def parse_protect(self, kw_protect, *tokens):
+        self.process_list(tokens, self.collect_device, self.protected)
+
+    def parse_isolate(self, kw_isolate, *tokens):
+        devices = []
+        self.process_list(tokens, self.collect_device, devices)
+        self.isolated.append(devices)
+
+    def collect_device(self, device, devices):
+        devices.append(device.str)
+
+    def parse_trusted(self, kw_trusted, kw_kind, *token):
+        if kw_kind.str == 'host':
+            self.trusted_hosts.append(ipaddress.ip_address(token[0].str))
+        elif kw_kind.str == 'network' or kw_kind.str == 'net':
+            self.trusted_networks.append(ipaddress.ip_interface(token[0].str))
+        else:
+            if not token:
+                if self.parent.nodedef.name != 'interface':
+                    raise RuntimeError('implicit trusted interface must be ' +
+                                       'nested under an interface')
+                else:
+                    name = self.parent.name
+            else:
+                name = token[0].str
+            self.trusted_interfaces.append(name)
 
     def parse_accept(self, kw_accept, *tokens):
         pass
@@ -14,13 +47,6 @@ class Firewall(Node):
     def parse_snat(self, kw_snat, *tokens):
         pass
 
-    def parse_isolate(self, kw_isolate, *tokens):
-        link_list = []
-        self.process_list(tokens, self.collect_link, link_list)
-        self.isolations.append(link_list)
-
-    def collect_link(self, link, link_list):
-        link_list.append(link.str)
 
     def finalize(self):
         Node.finalize(self)
@@ -30,16 +56,34 @@ class Firewall(Node):
 
 def generate_firewall(nodedef, nodes, fs):
     print('generate_firewall...')
+    fw = nodes[0]
+    for devices in fw.isolated:
+        if len(devices) > 1:
+            for src in devices:
+                for dst in devices:
+                    print('iptables -t filter -I FORWARD -i %s -o %s -j DROP' %
+                          (src, dst))
+        else:
+            src = dst = devices[0]
+            print('iptables -t filter -I FORWARD -i %s -o %s -j DROP' %
+                  (src, dst))
+
 
 NodeDef(
     'firewall', Firewall, 0,
-    Lexer.Keywords(['accept', 'drop', 'reject',
-                    'snat', 'dnat', 'on',
-                    'input', 'output', 'forward',
+    Lexer.Keywords(['protect', 'accept', 'drop', 'reject',
+                    'trusted', 'host', 'interface', 'net', 'network',
+                    'snat', 'dnat', 'input', 'output', 'forward',
                     'isolate']),
     Lexer.NoTokens(),
-    [Parser.Rule('_accept_ _token_( _token_)*'  , 'parse_accept' ),
-     Parser.Rule('_snat_ _on_ _token_( _token_)*'    , 'parse_snat'   ),
-     Parser.Rule('_isolate_ _token_(, _token_)*', 'parse_isolate')],
+    [Parser.Rule('_protect_ _token_(, _token_)*' , 'parse_protect'),
+     Parser.Rule('_isolate_ _token_(, _token_)*' , 'parse_isolate'),
+     Parser.Rule('_trusted_ (_interface_|_network_|_host_) _token_',
+                 'parse_trusted'),
+     Parser.Rule('_trusted_ (_interface_)', 'parse_trusted'),
+
+     Parser.Rule('_accept_ _token_( _token_)*'   , 'parse_accept' ),
+     Parser.Rule('_snat_ _token_( _token_)*', 'parse_snat'   )],
     generate_firewall
 )
+
