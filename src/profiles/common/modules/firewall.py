e8d7c8118b0b96212612a5ed9518ea25dc40f3fa
Krisztian Litkey <krisztian.litkey@intel.com>
2018-01-05 18:35:16 +0200

    WiP...

--- src/profiles/common/modules/firewall.py
+++ src/profiles/common/modules/firewall.py
@@ -3,6 +3,113 @@
 import ipaddress
 from genconfig.parser import *
 
+class IPTables:
+    class Table:
+        def __init__(self, name, chains = []):
+            self.name = name
+            self.chains = chains
+            self.index = {}
+            for idx in range(0, len(self.chains)):
+                self.index[self.chains[idx].name] = idx
+
+        def chain(self, name):
+            return self.chains[self.index[name]]
+
+        def add_chain(self, name, policy = None):
+            if name in keys(self.chains):
+                return self.chains[self.index[name]]
+            else:
+                self.chains.name = chain = Chain(name, policy)
+                self.index[chain.name] = len(self.chains)
+                return chain
+
+    class Marker:
+        def __init__(self, name):
+            self.name = name
+
+    class Chain:
+        def __init__(self, name, policy = None):
+            self.name = name
+            self.policy = policy
+            self.rules = []
+
+        def marker(self, name, index = -1):
+            if index > -1:
+                self.rules.insert(index, Marker(name))
+            else:
+                self.rules.append(Marker(name))
+        
+        def marker_index(self, name):
+            for idx in range(0, len(self.rules)):
+                if type(self.rules[idx]) == Marker:
+                    return idx
+            return -1
+
+    class Match:
+        def __init__(self, name, options):
+            self.name = name
+            self.options = options
+
+    class Rule:
+        def __init__(self, selectors, target, target_opts = None, matches = []):
+            self.selectors = selectors
+            self.matches = matches
+            self.target = target
+            self.target_options = target_opts
+            
+    def __init__(self, policy = None):
+        self.filter = IPTables.Table('filter', [
+            IPTables.Chain('INPUT', policy),
+            IPTables.Chain('FORWARD', policy),
+            IPTables.Chain('OUTPUT')])
+        self.nat = IPTables.Table('nat', [
+            IPTables.Chain('PREROUTING'),
+            IPTables.Chain('INPUT'),
+            IPTables.Chain('OUTPUT'),
+            IPTables.Chain('POSTROUTING')])
+        self.mangle = IPTables.Table('mangle', [
+            IPTables.Chain('PREROUTING'),
+            IPTables.Chain('INPUT'),
+            IPTables.Chain('FORWARD'),
+            IPTables.Chain('OUTPUT'),
+            IPTables.Chain('POSTROUTING')])
+        self.raw = IPTables.Table('raw', [
+            IPTables.Chain('PREROUTING'),
+            IPTables.Chain('OUTPUT')])
+        self.tables = {
+            'filter': self.filter,
+            'nat': self.nat,
+            'mangle': self.mangle,
+            'raw': self.raw
+        }
+
+    def table(self, name):
+        return self.tables[name]
+
+    def chain(self, tname, cname):
+        return table(tname).chain(cname)
+
+    def marker_index(self, tname, cname, mname):
+        return table(tname).chain(cname).marker_index(mname)
+
+    def insert(self, tbl_chain, *rule_args, after_marker = None):
+        tbl, chain = tbl_chain.split(':')
+        if after_marker:
+            idx = self.marker_index(tbl, chain, after_) + 1
+        else:
+            idx = 0
+        chain.rules.insert(idx, IPTables.Rule(rule_args))
+        
+    def append(self, tbl_chain, *rule_args, before_marker = None):
+        tbl, chain = tbl_chain.split(':')
+        if before_marker:
+            idx = self.marker_index(tbl, chain, before_marker = None)
+        else:
+            idx = len(chain.rules)
+        chain.rules.insert(idx, IPTables.Rule(rule_args))
+        
+
+
 class Firewall(Node):
     def __init__(self, nodedef, root, parent, node_tkn):
         Node.__init__(self, nodedef, root, parent, node_tkn)
@@ -56,6 +163,9 @@ class Firewall(Node):
 
 def generate_firewall(nodedef, nodes, fs):
     print('generate_firewall...')
+    ipt = IPTables()
+#    ipt.insert('filter:FORWARD', '', '', 'ACCEPT'
+               matches = [('conntrack', '--ctstate RELATED,ESTABLISHED')])
     fw = nodes[0]
     for devices in fw.isolated:
         if len(devices) > 1:
